9d901ec1a5e45ff07ed7e321bf035e406d473ac7
VulcanoAhab <vulcano.ahab@gmail.com>
2018-06-15 15:50:31 -0300

    Project refactoring

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="pySteer",
+    version='0.1.0',
+    author="VulcanoAhab",
+    packages=["pySteer"],
+    url="https://github.com/VulcanoAhab/pySteer.git",
+    description="Control Browsers",
+    install_requires=[
+        "PyYAML==3.12",
+        ]
+)
